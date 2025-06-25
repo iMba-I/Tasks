5f8ee693c0bcac09e3f6cf738b48f5502e515b86
iMba-I <68714664+iMba-I@users.noreply.github.com>
2025-06-25 15:42:34 +0300

    Update task2.sql

--- task2/task2.sql
+++ task2/task2.sql
@@ -1,3 +1,16 @@
+DELETE FROM products
+WHERE NOT EXISTS (
+    SELECT product_id
+    FROM availabilities
+    WHERE availabilities.product_id = products.id
+    );
+
+DELETE FROM stocks
+WHERE NOT EXISTS (
+    SELECT stock_id
+    FROM availabilities
+    WHERE availabilities.stock_id = stocks.id
+    );
 
 DELETE FROM categories
 WHERE NOT EXISTS (
@@ -6,16 +19,4 @@ WHERE NOT EXISTS (
     WHERE categories.id = products.category_id
     );
     
-DELETE FROM products
-WHERE NOT EXISTS (
-    SELECT product_id
-    FROM availabilities
-    WHERE availabilities.product_id = products.id
-    );
     
-DELETE FROM stocks
-WHERE NOT EXISTS (
-    SELECT stock_id
-    FROM availabilities
-    WHERE availabilities.stock_id = stocks.id
-    )
