6daae4b0d2709478855722cfb1849142af5a17a9
iMba-I <68714664+iMba-I@users.noreply.github.com>
2025-06-25 18:33:22 +0300

    Add files via upload

--- task5/task5.php
+++ task5/task5.php
@@ -1,1 +1,18 @@
+<?php
 
+function howmanysisters($num_sis, $num_broth)
+{
+	if (!is_numeric($num_sis) || !is_numeric($num_broth))
+		return "Input must be numbers";
+	$num_sis = (int)$num_sis;
+	$num_broth = (int)$num_broth;
+
+	return $num_sis +1;
+}
+
+echo howmanysisters(1.752512,5) . '<br>';
+echo howmanysisters('11111',5) . '<br>';
+echo howmanysisters('asfasfassafwafas',5) . '<br>';
+echo howmanysisters(1,'asfasfasfasfasfas') . '<br>';
+
+  ?>
